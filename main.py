293f45dd36d71e4b669f549f192317e9911cd6c2
Henry <henrytang787@gmail.com>
2021-05-01 20:06:18 -0500

    create and store files in output folder

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import os
 import pathlib
 import getpass
+from shutil import copyfile
 
-# Current user
+# Get current user
 username = getpass. getuser()
 
-# Top folders 
+# Target folders
 desktop = os.path.expanduser("~\Desktop")
 documents = os.path.expanduser("~\Documents")
+
+# Create output folder
+outputFolder = os.getcwd() + '\output'
+
+if not os.path.exists(outputFolder):
+    os.makedirs(outputFolder)
+
 
 # Testing functions
 print(desktop)
 print(documents)
+print(outputFolder)
 
+# Find all folders and subfolders of target user
 for root, subdirs, files in os.walk(desktop):
-    for d in subdirs:
-        print(d)
-
-        
+    for name in files:
+        dest = outputFolder + "\\" + name 
+        print(dest)
+        copyfile(os.path.join(root, name), dest)
+        # print("")
